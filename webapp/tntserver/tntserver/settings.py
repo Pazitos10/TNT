28264a2451a7b5075fe2101ecc2118710c0f9268
fdmansilla333 <fdmansilla333@gmail.com>
2016-05-27 10:57:48 -0300

    Agregado rest_framework

--- webapp/tntserver/tntserver/settings.py
+++ webapp/tntserver/tntserver/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tntapp'
+    'tntapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+#Agregado rest_framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
