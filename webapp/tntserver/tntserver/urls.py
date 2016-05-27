626770a027ae64b8def8e06c8e874b1b20c4bb83
fdmansilla333 <fdmansilla333@gmail.com>
2016-05-27 10:58:42 -0300

    Agregado de api-auth a framework

--- webapp/tntserver/tntserver/urls.py
+++ webapp/tntserver/tntserver/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^', include('tntapp.urls', namespace='materias')),
     url(r'^materias/', include('tntapp.urls', namespace='materias')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
